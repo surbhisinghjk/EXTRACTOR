f217c54fe68053e5c2c2ed3c2e7ed14c958a65af
Surbhi Singh <surbhisinghtyu36@gmail.com>
2024-09-08 22:06:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "18618422"))
 API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
-BOT_TOKEN = getenv("BOT_TOKEN", "7070221680:AAHdf_-TvaUTvgEAl6pa0IGAjD72hPAWaW4")
+BOT_TOKEN = getenv("BOT_TOKEN", "707022168")
 OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 2112898623").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
